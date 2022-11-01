a5b37796be8be1074515efe931456b25ee3e6be2
Gunars Leitans <92DPGLeitans@rvt.lv>
2022-11-01 16:27:22 +0200

    commit for git pages

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Burtnieks
+# LC
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from blueprints.auth.views import bp as bp_auth
+
+app = Flask(__name__)
+app.secret_key = 'qwerty123'
+app.debug = True
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+app.register_blueprint(bp_auth)
